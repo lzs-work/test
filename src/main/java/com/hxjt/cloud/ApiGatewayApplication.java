48ee2db1c9474c4b8300ad8d3aacdb935933c052
lzs <lzs187@163.com>
2019-11-03 21:40:35 +0800

    创建分支dev5

--- src/main/java/com/hxjt/cloud/ApiGatewayApplication.java
+++ src/main/java/com/hxjt/cloud/ApiGatewayApplication.java
@@ -16,7 +16,7 @@ public class ApiGatewayApplication {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
         System.out.println("分支修改");
-        System.out.println("dev3");
+        System.out.println("dev5");
         return  new TokenFilter();
     }
 
